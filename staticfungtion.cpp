71ef7a5f7963d0f6873955f8f316ead7c4b0b515
Naufal Arif <na713634@gmail.com>
2026-05-05 16:49:48 +0700

    class Mahasiswa

---
+++ staticfungtion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string> // Menyertakan library string
+using namespace std;
+
+class Mahasiswa {
+private:
+    static int nim; // Variabel static shared
+
+public:
+    int id;
+    string nama;
+
+    // Prosedur yang akan didefinisikan di luar class
+    void setID();
+    void printAll();
+
+    // Static function untuk memanipulasi variabel static
+    static void setNim(int pnim) {
+        nim = pnim;
+    }
+
+    static int getNim() {
+        return nim;
+    }
+
+    // Constructor dengan member initialize list
+    Mahasiswa(string pnama) : nama(pnama) {
+        setID();
+    }
+};
