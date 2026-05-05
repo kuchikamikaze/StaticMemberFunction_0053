ef017cc217b3947917d49d150406fd509c76c0e6
Naufal Arif <na713634@gmail.com>
2026-05-05 17:08:16 +0700

    contructor, destructor, and fungtion input and output

--- destructor.cpp
+++ destructor.cpp
@@ -13,3 +13,34 @@ class angka
     void cetakData();
     void isiData();
 };
+
+angka::angka(int i) {
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << "=" << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
