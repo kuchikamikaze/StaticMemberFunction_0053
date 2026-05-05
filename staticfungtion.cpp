b9c59e1700a761764c9b9e8a570154b764f3af54
Naufal Arif <na713634@gmail.com>
2026-05-05 16:50:43 +0700

    assign value to member

--- staticfungtion.cpp
+++ staticfungtion.cpp
@@ -28,3 +28,17 @@ public:
         setID();
     }
 };
+// Inisialisasi variabel static di luar class
+int Mahasiswa::nim = 0;
+
+// Implementasi prosedur setID di luar class
+void Mahasiswa::setID() {
+    id = ++nim; // Prefix increment agar NIM naik sebelum diassign
+}
+
+// Implementasi prosedur printAll di luar class
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
