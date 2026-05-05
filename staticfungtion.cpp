f6abc2c31f8b0cb3691a2ad0a6e00628f2d0a2ba
Naufal Arif <na713634@gmail.com>
2026-05-05 16:51:51 +0700

    fungtion main

--- staticfungtion.cpp
+++ staticfungtion.cpp
@@ -42,3 +42,28 @@ void Mahasiswa::printAll() {
     cout << "Nama = " << nama << endl;
     cout << endl;
 }
+
+int main() {
+    // Membuat object dan memberi nilai awal
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+
+    // Mengubah nilai shared NIM menggunakan static function
+    Mahasiswa::setNim(9);
+
+    // Object yang dibuat setelah setNim(9) akan melanjutkan dari angka tersebut
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    // Menampilkan semua data mahasiswa
+    mhs1.printAll(); // ID: 1
+    mhs2.printAll(); // ID: 2
+    mhs3.printAll(); // ID: 10 (9 + 1)
+    mhs4.printAll(); // ID: 11 (10 + 1)
+
+    // Menampilkan status NIM terakhir melalui akses static function
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    
+    system("pause"); // Menghentikan console agar tidak langsung tertutup
+    return 0;
+}
